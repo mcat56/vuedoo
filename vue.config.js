1d6a6604109632f1c4b5c50876a87a647973fc06
Mary Lang <Mary.Lang@vizio.com>
2020-07-06 13:35:43 -0600

    add pwa config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
+  pwa: {
+    workboxPluginMode: "InjectManifest",
+    workboxOptions: {
+      swSrc: "src/service-worker.js"
+    },
+    themeColor: "#1da025"
+  },
   transpileDependencies: ["vuetify"]
 };
