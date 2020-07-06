949d783ab1791866d304e92b1e8ce8c902760daa
Mary Lang <Mary.Lang@vizio.com>
2020-07-06 15:25:20 -0600

    toggle state in store

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,12 +5,12 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    userComponents: Array<string>() 
+    userComponents: Array<string>()
   },
   mutations: {
     updateComponents(state, component) {
       if (state.userComponents.includes(component)) {
-        state.userComponents.splice(state.userComponents.indexOf(component),1)
+        state.userComponents.splice(state.userComponents.indexOf(component), 1);
       } else {
         state.userComponents.push(component);
       }
