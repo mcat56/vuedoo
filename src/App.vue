62a72f50360587970878658104750b9c47769510
Mary Lang <Mary.Lang@vizio.com>
2020-07-07 17:07:38 -0600

    Add siderail to App

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <div id="app">
+    <side-rail
+      v-show="$store.state.sideRail"
+      :style="{ width: sideRailWidth, height: sideRailHeight }"
+    />
     <router-view />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import SideRail from "@/components/SideRail.vue";
+import { mapState } from "vuex";
 
-@Component({})
+@Component({
+  components: { SideRail },
+  computed: mapState(["sideRailWidth", "sideRailHeight"])
+})
 export default class App extends Vue {}
 </script>
 <style lang="scss">
@@ -17,5 +26,6 @@ export default class App extends Vue {}
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin-left: 320px;
 }
 </style>
