615e3817cb89c0a9203d0cac8811357a19f5a605
Mary Lang <Mary.Lang@vizio.com>
2020-07-07 17:07:56 -0600

    Create siderail component

---
+++ src/components/SideRail.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="siderail">
+    <h2>Side Rail Container</h2>
+    <button class="button" v-on:click="$store.commit('closeSideRail')">
+      Close
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  name: "SideRail",
+  props: {
+    width: {
+      type: String
+    },
+    height: {
+      type: String
+    }
+  }
+})
+export default class SideRail extends Vue {}
+</script>
+
+<style lang="scss">
+$top-color: black;
+$bottom-color: grey;
+
+.siderail {
+  background: -webkit-linear-gradient($top-color, $bottom-color);
+  background: -o-linear-gradient($top-color, $bottom-color);
+  background: -moz-linear-gradient($top-color, $bottom-color);
+  background: linear-gradient($top-color, $bottom-color);
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  overflow-x: hidden;
+  padding-top: 20px;
+}
+
+h2 {
+  color: white;
+}
+
+.button {
+  color: white;
+  background-color: rgb(72, 83, 92);
+  border-radius: 8px;
+  position: absolute;
+  bottom: 20px;
+  left: 130px;
+}
+
+.button:hover {
+  background-color: black;
+}
+</style>
