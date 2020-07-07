c12da3ada79ab6d69385c6a640cfb0fe04a794ac
Mary Lang <Mary.Lang@vizio.com>
2020-07-07 17:08:22 -0600

    Add height width and dimension mutations to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,7 +5,12 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    userComponents: Array<string>()
+    userComponents: Array<string>(),
+    width: '320px',
+    height: '720px',
+    sideRailWidth: '320px',
+    sideRailHeight: '720px',
+    sideRail: true
   },
   mutations: {
     updateComponents(state, component) {
@@ -14,6 +19,17 @@ export default new Vuex.Store({
       } else {
         state.userComponents.push(component);
       }
+    },
+    updateSiderail(state, dimensions) {
+      state.sideRailWidth = dimensions.width;
+      state.sideRailHeight = dimensions.height;
+    },
+    updateAppDimensions(state, dimensions) {
+      state.width = dimensions.width,
+      state.height = dimensions.height
+    },
+    closeSideRail(state) {
+      state.sideRail = false
     }
   },
   actions: {},
