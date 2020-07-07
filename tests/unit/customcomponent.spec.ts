67d56f685ebdf09dc788bd4737d260157d34310c
mcat56 <mcl86@cornell.edu>
2020-07-07 09:51:10 -0600

    break out customcomponent with beforeEach

--- tests/unit/customcomponent.spec.ts
+++ tests/unit/customcomponent.spec.ts
@@ -6,27 +6,30 @@ import store from "@/store";
 const localVue = createLocalVue()
 localVue.use(BootstrapVue)
 
+let wrapper: any; 
+
+beforeEach(() => {
+  wrapper =  shallowMount(CustomComponent, {
+    localVue, propsData: { name: 'Navigation Bar', 
+    img: 'https://i.imgur.com/wlwZgAT.png', 
+    componentName: 'navbar'}, store
+  });
+});
+
+afterEach(() => {
+  wrapper.destroy();
+})
 describe("CustomComponent.vue", () => {
   it("renders props.name when passed", () => {
-      const wrapper = shallowMount(CustomComponent, {
-          localVue, propsData: { name: 'Navigation Bar', 
-          img: 'https://i.imgur.com/wlwZgAT.png', 
-          componentName: 'navbar'}, store
-      })
       expect(wrapper.classes('CustomComponent'))
       expect(wrapper.text()).toMatch(name);
     });
     it("has a link", () => {
-    const wrapper = shallowMount(CustomComponent, {
-        localVue, propsData: { name: 'Navigation Bar', 
-        img: 'https://i.imgur.com/wlwZgAT.png', 
-        componentName: 'navbar'}, store
-    })
-
     const row = document.querySelector('b-row')
     expect(row).toBeDefined
     expect(wrapper.html()).toContain('<b-row')
     expect(wrapper.findComponent({ name: 'BLink'}).exists()).toBe(true)
+    // why doesnt BRow exist?
     // expect(wrapper.findComponent({ name: 'BRow').exists()).toBe(true)
     expect(wrapper.text()).toMatch('Navigation Bar')
   })
